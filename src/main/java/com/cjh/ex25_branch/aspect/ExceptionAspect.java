0856502f744d1cd12da066d67f7ce1fabfcb2da8
jihyeon1011 <jihyeon1619@gmail.com>
2025-10-28 16:46:34 +0900

    add aop

---
+++ src/main/java/com/cjh/ex25_branch/aspect/ExceptionAspect.java
@@ -0,0 +1,19 @@
+package com.cjh.ex25_branch.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class ExceptionAspect {
+
+	@AfterThrowing(pointcut = "execution(* com.cjh.ex25_branch.service.*.*(..))", throwing = "excep")
+	public void logError(JoinPoint jp, Exception excep) {
+		log.info("rrrrr 메서드중 오류\n 예외발생위치: {}, \n오류상황: {} rrrrr end rrrrr", jp.getSignature(), excep.getMessage());
+	}
+}
